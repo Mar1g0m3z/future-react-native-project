ee1b329b8edc495bdf4b16572e8b0b933f03cf4b
Marisela Gomez <mariselaalejandragomez@gmail.com>
2024-04-23 16:40:19 -0700

    control flows

---
+++ control-flow.js
@@ -0,0 +1,69 @@
+// Conditional Statements
+// if...else
+
+// Hour
+// if hour is between 6 am and 12pm: good morning!
+// if hour between 12pm and 6pmL good afternoon!
+// otherwise: good evening!
+
+// Block of code
+// if (condition) {
+//     statement
+// }
+// else if(anothercondition){
+// statement
+// }
+// else {statement}
+
+let hour = 10;
+
+if (hour >= 6 && hour < 12) {
+	console.log('Good morning');
+} else if (hour >= 12 && hour < 18) {
+	console.log('Good Afternoon');
+} else {
+	console.log('Good Evening');
+}
+
+// Switch...Case
+let role = 'guest';
+switch (role) {
+	case 'guest':
+		console.log('guest user');
+		break;
+	case 'moderator':
+		console.log('moderator user');
+		break;
+	case 'admin':
+		console.log('admin user');
+		break;
+	default:
+		console.log('unknown user');
+}
+
+//just use if...then lol
+
+//LOOPS
+// repeat action number of times
+// For
+for (let i = 0; i < 5; i++) {
+	if (i % 2 !== 0) {
+		console.log(i);
+	}
+}
+// you can also iterate backwards
+for (let i = 5; i >= 1; i--) {
+	if (i % 2 !== 0) {
+		console.log(i);
+	}
+}
+
+// the loop executes as long as the condition is true (less than 5) then i is increased by 1
+
+// WHILE loop
+// in for loops the loop variable is inside the loop but in while loop it is external
+let i = 0;
+while (i <= 5) {
+	if (i % 2 !== 0) console.log(i);
+	i++;
+}
