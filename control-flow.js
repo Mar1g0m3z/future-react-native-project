de4cc6bf607b5b4972732fd87a6c91c4570c8f56
Marisela Gomez <mariselaalejandragomez@gmail.com>
2024-05-01 23:44:29 -0700

    more basic js for commiting needs.

--- control-flow.js
+++ control-flow.js
@@ -183,7 +183,16 @@ console.log(fizzBuzz(true));
 
 function checkSpeed(speed) {
 	const speedLimit = 70;
+	const kmPerPoint = 5;
 	if (speed < speedLimit) {
-		return 'Ok';
+		console.log('Ok');
+	} else {
+		let points = Math.floor((speed - speedLimit) / kmPerPoint);
+		if (points >= 12) console.log('Liscense suspended');
+		else {
+			console.log('points', points);
+		}
 	}
 }
+
+checkSpeed(33);
