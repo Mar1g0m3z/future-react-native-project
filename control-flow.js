73e13d19c686431f65e4efad8ba7025adc248f5b
Marisela Gomez <mariselaalejandragomez@gmail.com>
2024-05-12 15:38:13 -0700

    uhh

--- control-flow.js
+++ control-flow.js
@@ -198,19 +198,33 @@ console.log(fizzBuzz(true));
 
 // checkSpeed(75);
 
-function showNumbers(limit) {
-	// for (let i = 0; i <= limit; i++) {
-	// 	if (i % 2 === 0) {
-	// 		console.log(i, 'EVEN');
-	// 	} else {
-	// 		console.log(i, 'ODD');
-	// 	}
-	// }
-	// OR
-	for (let i = 0; i <= limit; i++) {
-		const message = i % 2 === 0 ? 'EVEN' : 'ODD';
-		console.log(i, message);
+// function showNumbers(limit) {
+// 	// for (let i = 0; i <= limit; i++) {
+// 	// 	if (i % 2 === 0) {
+// 	// 		console.log(i, 'EVEN');
+// 	// 	} else {
+// 	// 		console.log(i, 'ODD');
+// 	// 	}
+// 	// }
+// 	// OR
+// 	for (let i = 0; i <= limit; i++) {
+// 		const message = i % 2 === 0 ? 'EVEN' : 'ODD';
+// 		console.log(i, message);
+// 	}
+// }
+
+// showNumbers(12);
+
+function countTruthy(array) {
+	let count = 0;
+	for (let i = 0; i < array.length; i++) {
+		if (array[i]) {
+			count++;
+		}
 	}
+	return count;
 }
 
-showNumbers(12);
+const array = [0, '', 3, NaN, 1];
+
+console.log(countTruthy(array));
