89ce193d7b49e3320524377d1c95745ea6dc73e6
Marisela Gomez <mariselaalejandragomez@gmail.com>
2024-06-03 20:37:09 -0700

    object start

--- objects.js
+++ objects.js
@@ -1,3 +1,19 @@
 // just making this comment to get app academy up my butt
 // making me do shit every week tf
 // this should be enough lol
+
+//OOP
+// a program a collection of objects that talk to each other
+const circle = {
+	radius: 1,
+	location: {
+		x: 1,
+		y: 1,
+	},
+	isVisible: true,
+	draw: function () {
+		console.log('draw');
+	},
+};
+
+circle.draw(); //draw method: is a function part of an object
